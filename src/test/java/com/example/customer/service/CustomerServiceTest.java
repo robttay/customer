721d2d57a130a31a88407191d414d2f2f1f3008f
robttay <robttay@yahoo.com>
2017-08-07 15:26:47 -0500

    added more tests

--- src/test/java/com/example/customer/service/CustomerServiceTest.java
+++ src/test/java/com/example/customer/service/CustomerServiceTest.java
@@ -20,26 +20,70 @@ public class CustomerServiceTest {
     CustomerService customerService;
 
     @Test
-    public void testTransactional() {
-        Customer customer1 = createTestCustomer();
-        Customer customer2 = createTestCustomer();
-        List<Customer> customers = new ArrayList<>();
-        customers.add(customer1);
-        customers.add(customer2);
+    public void testAddGet() {
+        // Get unique names every time this test runs
+        String firstName = Long.toString(System.currentTimeMillis());
+        String lastName = Long.toString(System.currentTimeMillis());
 
-        customer2.setFirstName(null);
-        try {
-            customerService.add(customer2);
-        } catch(Exception e) {
-        }
+        Customer customer1 = new Customer();
+        customer1.setFirstName(firstName);
+        customer1.setLastName(lastName);
+        customerService.add(customer1);
+
+        List<Customer> customers = customerService.get();
+
+        Customer customer2 = findInList(customers, firstName, lastName);
+        Assert.assertNotNull(customer2);
+
+        Customer customer3 = customerService.getById(customer2.getId());
+        Assert.assertNotNull(customer3);
+        Assert.assertEquals(firstName, customer3.getFirstName());
+        Assert.assertEquals(lastName, customer3.getLastName());
+    }
+
+    @Test
+    public void testUpdate() {
+        Customer customer1 = createTestCustomer();
+        customerService.add(customer1);
+
+        List<Customer> customers = customerService.get();
+
+        Customer customer2 = findInList(customers, customer1.getFirstName(), customer1.getLastName());
+        Assert.assertNotNull(customer2);
+
+        String updateFirstName = Long.toString(System.currentTimeMillis());
+        String updateLastName = Long.toString(System.currentTimeMillis());
+
+        customer2.setFirstName(updateFirstName);
+        customer2.setLastName(updateLastName);
+        customerService.update(customer2);
+
+        customers = customerService.get();
+
+        Customer customer3 = findInList(customers, updateFirstName, updateLastName);
+        Assert.assertNotNull(customer3);
+        Assert.assertEquals(customer2.getId(), customer3.getId());
+    }
+
+    @Test
+    public void testDelete() {
+        Customer customer1 = createTestCustomer();
+        customerService.add(customer1);
+
+        List<Customer> customers = customerService.get();
+
+        Customer customer2 = findInList(customers, customer1.getFirstName(), customer1.getLastName());
+        Assert.assertNotNull(customer2);
+
+        customerService.delete(customer2.getId());
 
         customers = customerService.get();
         Customer customer3 = findInList(customers, customer1.getFirstName(), customer1.getLastName());
-        Assert.assertNull("The first customer created should have rolled back", customer3);
+        Assert.assertNull(customer3);
     }
 
-
     private Customer createTestCustomer() {
+        // Get unique names every time this test runs
         String firstName = Long.toString(System.currentTimeMillis());
         String lastName = Long.toString(System.currentTimeMillis());
 
@@ -51,6 +95,7 @@ public class CustomerServiceTest {
 
 
     private Customer findInList(List<Customer> customers, String first, String last) {
+        // Find the new person in the list
         for (Customer customer : customers) {
             if (customer.getFirstName().equals(first) && customer.getLastName().equals(last)) {
                 return customer;
